73a3cf37bb246eb6db079319e6322928429fcfa7
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2026-03-16 15:11:22 +0000

    fix: restaurar configuracao base do karma

--- frontend/karma.conf.cjs
+++ frontend/karma.conf.cjs
@@ -1,13 +1,31 @@
+const path = require('path');
+
 module.exports = function (config) {
   config.set({
-    browsers: ['ChromeHeadless'],
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: path.join(__dirname, 'coverage/frontend'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcovonly' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage'],
       },
     },
-    singleRun: false,
     restartOnFileChange: true,
   });
 };
